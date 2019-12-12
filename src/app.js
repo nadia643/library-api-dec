67d9cf6bb3331280f05feed46081353d960a3d3a
John Doe <nadia643@gmail.com>
2019-12-12 16:52:13 +0000

    set up done

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
-const app = require('./app.js';)
 
 mongoose.connect(
-    process.env.DATABASE_CONN,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    () => {
-      app.listen(3000);
-    }
-  );
+  process.env.DATABASE_CONN,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    app.listen(3000);
+  }
+);
