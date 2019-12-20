d8592e307f86f9558d86dd4c9ce1e8adec22f15d
John Doe <nadia643@gmail.com>
2019-12-20 16:57:13 +0000

    before each hook started

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,12 +1,13 @@
-// Write a beforeEach hook that removes all data from the database 
-// before each test Add a test that does the following:
-
-//     Makes a POST request to your server, sending a JSON body with 
-//     firstName, lastName, email and password fields Asserts that the 
-//     response status indicates that a new resource has been created
-//     Uses the _id field from the response body to find the created 
-//     User document in the database  Asserts that the created user 
-//     has the correct firstName, lastName, email and password properties
-
-// Write the code to make this test pass
-
+beforeEach(() => {
+    console.log('beforeEach')
+  })
+  
+  describe('tests', () => {
+    before(() => {
+      console.log('before')
+    })
+  
+    it('test 1', () => {
+      console.log('it')
+    })
+  })
